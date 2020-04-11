8038486e7c33e9daaba70eb743ff9a2d4f6bdb54
Matt McCredie <mccredie+git@gmail.com>
2020-04-11 13:40:26 -0700

    Adding a table component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Table from "./components/Table"
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <Table />
     </div>
   );
 }
