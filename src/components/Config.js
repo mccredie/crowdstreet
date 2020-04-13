3a71a3c2248f210012b6926feac308854738d66a
Matt McCredie <mccredie+git@gmail.com>
2020-04-12 21:11:38 -0700

    Adding input validation.

--- src/components/Config.js
+++ src/components/Config.js
@@ -4,23 +4,50 @@ import classNames from "classnames";
 
 import "./Config.css";
 
+const number = (value, allValues, props, name) => {
+    const asNumber = parseInt(value);
+    if (!Number.isInteger(asNumber) || !Number.isFinite(asNumber)) {
+        return `The value of ${name} should be an integer`
+    }
+}
+
+const normInt = (value) => {
+    const intValue = parseInt(value);
+    if (isNaN(intValue)) {
+        return value;
+    }
+    return intValue;
+}
+
+const renderField = ({
+  input,
+  meta: { touched, error },
+  ...rest
+}) => (
+  <>
+      <input {...input} {...rest}/>
+      {touched &&
+        (error && <span>{error}</span>)}
+  </>
+);
+
 export default ({ className, handleSubmit, onSubmit, onCancel }) => (
     <form className={classNames("Config", className)} onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label htmlFor="n">N = </label>
-        <Field name="n" component="input" type="text"  parse={(value) => parseInt(value)} />
+        <label htmlFor="n">N =</label>
+        <Field name="n" component={renderField} type="text" normalize={normInt} validate={number} />
       </div>
       <div>
         <label htmlFor="x">X = </label>
-        <Field name="x" component="input" type="text" parse={(value) => parseInt(value)} />
+        <Field name="x" component={renderField} type="text" normalize={normInt} validate={number}/>
       </div>
       <div>
         <label htmlFor="m">M = </label>
-        <Field name="m" component="input" type="text" parse={(value) => parseInt(value)} />
+        <Field name="m" component={renderField} type="text" normalize={normInt} validate={number}/>
       </div>
       <div>
         <label htmlFor="w">W = </label>
-        <Field name="w" component="input" type="text" parse={(value) => parseInt(value)} />%
+        <Field name="w" component={renderField} type="text" normalize={normInt} validate={number}/>%
       </div>
       <div>
         <label htmlFor="d">D = </label>
